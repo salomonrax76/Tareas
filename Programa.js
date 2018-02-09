fbf1a71481ab6396c60774a3f819217aad82b788
Salomon Rax <salomonrax@gmail.com>
2018-02-09 12:54:33 -0600

    Subiendo mi archivo

---
+++ Programa.js
@@ -0,0 +1,8 @@
+var nom, ed, nac;   
+nom=prompt('Ingrese nombre: ','');
+ed=prompt('Ingrese edad: ','');
+nac=2018-ed;
+if (ed>=18)      
+  alert("Hola "+nom+" usted tiene "+ed+" años, nació en "+nac+ " y es mayor de edad"); 
+else
+  alert("Hola "+nom+" usted tiene "+ed+" años, nació en "+nac+ " y es menor de edad"); 
